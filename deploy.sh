d57df541631223c7b74992d7eb8c9d31ce7887ee
Peter Inglesby <peter.inglesby@gmail.com>
2015-06-21 15:56:47 +0100

    hseafse

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-echo TRAVIS_BRANCH: $TRAVIS_BRANCH
-echo TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST
-echo TRAVIS: $TRAVIS
-
-echo "config before:"
-git config --list
-
 if [[ $TRAVIS = "true" ]]; then
 	if [[ $TRAVIS_BRANCH != "master" || $TRAVIS_PULL_REQUEST != "false" ]]; then
 		# Bail out if Travis is building a branch or is building a Pull Request.
@@ -16,55 +9,37 @@ if [[ $TRAVIS = "true" ]]; then
 		exit 0
 	fi
 
-	echo "...A"
 	# Set up credentials for pushing to GitHub.  $GH_TOKEN is configured via Travis web UI.
-	git config credential.helper "store --file=.git/credentials"
-	echo "...B"
+	git config --global credential.helper "store --file=.git/credentials"
 	echo "https://inglesp:$GH_TOKEN@github.com" > .git/credentials
 
 	# Set up config for committing.
-	echo "...C"
-	git config user.name "Travis"
-	echo "...D"
-	git config user.email "no-reply@pyconuk.org"
-	echo "...E"
+	git config --global user.name "Travis"
+	git config --global user.email "no-reply@pyconuk.org"
 fi
-
-echo "config after:"
-git config --list
 
 echo "Deploying!"
 
 # Remove output directory and replace it with the current tip of the gh-pages branch.
 rm -rf output
-echo "...F"
 git clone https://github.com/inglesp/deploy-test-3 --branch gh-pages --single-branch output
-echo "...G"
 
 # Update the output directory with recent changes.
 wok
-echo "...H"
 
 # Add and commit any changes.
 cd output
-echo "...I"
 git add .
-echo "...J"
 git commit -m "[skip ci]  Auto-commit.  Built latest changes"
-echo "...K"
 
 if [[ $TRAVIS = "true" ]]; then
 	# Push to GitHub.
 	git push https://inglesp@github.com/inglesp/deploy-test-3 gh-pages
-	echo "...L"
 else
 	# Push to GitHub.
 	git push
-	echo "...M"
 fi
 
 # Clean up.
 rm -rf .git
-echo "...N"
 cd ..
-echo "...O"
