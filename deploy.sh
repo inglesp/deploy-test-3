c0df7dce7265a75d6e50af5a1a81952214e1e779
Peter Inglesby <peter.inglesby@gmail.com>
2015-06-21 14:38:19 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,46 @@
 #!/bin/bash
 
-if [[ $TRAVIS = "true" ]]; then
-	if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
-		echo "Deploying!"
+set -e
 
-		# Set up credentials for pushing to GitHub.  $GH_TOKEN is
-		# configured via Travis web UI.
-		git config credential.helper "store --file=.git/credentials"
-		echo "https://inglesp:$GH_TOKEN@github.com" > .git/credentials
-		git config user.name "Travis"
-		git config user.email "travis@travis-ci.org"
-
-		# Add, commit, and push any changes to the output directory
-		# introduced by this change.  The output directory will have
-		# been updated if required when pre-flight-checks.sh ran.  If
-		# the output directory is already up to date then no new commit
-		# will be made.
-		echo "Checking out master"
-		git checkout master
-
-		echo ".gitignore before:"
-		cat .gitignore
-		sed -i /^output/d .gitignore
-		echo ".gitignore after:"
-		cat .gitignore
-
-		echo "Committing"
-		git add output
-		git commit -m "[skip ci] Travis auto-commit.  Built latest changes."
-
-		echo "Pushing"
-		git push https://inglesp@github.com/inglesp/deploy-test-3 master
-
-		# Push output directory to gh-pages branch on GitHub.
-		git subtree push --prefix output https://inglesp@github.com/inglesp/deploy-test-3 gh-pages
-
-		# Clean up.
-		rm .git/credentials
-	else
-		echo "Not deploying!"
-	fi
-else
-	git subtree push --prefix output origin gh-pages
+if [[ $TRAVIS = "true" && $TRAVIS_BRANCH != "master" ]]; then
+	# Bail out if Travis is not building master.  This happens when Travis
+	# is building a Pull Request.
+	echo "Not deploying!"
+	exit 0
 fi
+
+echo "Deploying!"
+
+# Remove output directory and replace it with the current tip of the gh-pages
+# branch.
+rm -rf output
+git clone https://github.com/inglesp/deploy-test-3.org --branch gh-pages --single-branch output
+
+# Update the output directory with recent changes.
+make build
+
+# Add and commit any changes.
+pushd output
+git add .
+git commit -m "[skip ci]  Auto-commit.  Built latest changes"
+
+if [[ $TRAVIS = "true" ]]; then
+	# Set up credentials for pushing to GitHub.  $GH_TOKEN is configured
+	# via Travis web UI.
+	git config credential.helper "store --file=.git/credentials"
+	echo "https://inglesp:$GH_TOKEN@github.com" > .git/credentials
+
+	# Set up config for committing.
+	git config user.name "Travis"
+	git config user.email "no-reply@pyconuk.org"
+
+	# Push to GitHub.
+	git push https://inglesp@github.com/inglesp/deploy-test-3.org gh-pages
+else
+	# Push to GitHub.
+	git push
+fi
+
+# Clean up.
+rm -rf .git
+popd
